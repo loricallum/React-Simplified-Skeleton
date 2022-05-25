11bdcbb0ea776d8f8832a1fc2f214b29d270cfa8
Lori C <lorimlewis@gmail.com>
2022-05-25 06:55:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  
+} from 'react-router-dom'
+import Layout from './layout/Layout'
+import Home from './components/pages/Home'
+import AboutUs from './components.pages/AboutUs'
+import ContactUs from '.cmponents/pages/ContactUs'
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path={'/AboutUs'} component={AboutUs}></Route>
+            <Route path={'/ContactUs'} component={ContactUs}></Route>
+            <Route path={'/'} component={Home}></Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
