25f162ad7320f7cd9626f57cb0cc1a4ce7864532
Lori C <lorimlewis@gmail.com>
2022-05-27 06:44:02 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-  
-} from 'react-router-dom'
-import Layout from './layout/Layout'
-import Home from './components/pages/Home'
-import AboutUs from './components.pages/AboutUs'
-import ContactUs from '.cmponents/pages/ContactUs'
+import { Route, Switch, useLocation } from "react-router"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
 
-import './App.css';
+
+//Components
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
+
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Layout>
-          <Switch>
-            <Route path={'/AboutUs'} component={AboutUs}></Route>
-            <Route path={'/ContactUs'} component={ContactUs}></Route>
-            <Route path={'/'} component={Home}></Route>
-          </Switch>
-        </Layout>
-      </Router>
-    </div>
-  );
+
+  const location = useLocation();
+  return <>
+
+
+  <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+
+    <SoundBar />
+
+{/* For framer-motion animation on page change! */}
+<AnimatePresence exitBeforeEnter>
+<Switch  location={location} key={location.pathname}>
+      <Route exact path="/" component={Main}/>
+      <Route exact path="/about" component={AboutPage}/>
+      <Route exact path="/blog" component={BlogPage}/>
+      <Route exact path="/work" component={WorkPage}/>
+      <Route exact path="/skills" component={MySkillsPage}/>
+
+    </Switch>
+</AnimatePresence>
+    
+    
+    </ThemeProvider>
+
+
+    
+    </>
+    
 }
 
-export default App;
+export default App
+
